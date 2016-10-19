3898d015b54166d1b03a477447a43f855d5b7fff
Carlos Miguens <cmiguens@gmail.com>
2016-10-19 17:13:30 -0300

    Two ultrasonic sensors implemented

---
+++ medidorDistancia/Debug/Generated_Code/TRIG_US_Back.d
@@ -0,0 +1,26 @@
+Generated_Code/TRIG_US_Back.o: ../Generated_Code/TRIG_US_Back.c \
+ ../Generated_Code/TRIG_US_Back.h ../Generated_Code/PE_Types.h \
+ ../Generated_Code/PE_Error.h ../Generated_Code/PE_Const.h \
+ ../Generated_Code/IO_Map.h \
+ /home/charly/workspace/medidorDistancia/Static_Code/IO_Map/MKL25Z4.h \
+ /home/charly/workspace/medidorDistancia/Static_Code/PDD/GPIO_PDD.h \
+ /home/charly/workspace/medidorDistancia/Static_Code/PDD/PDD_Types.h \
+ ../Generated_Code/Cpu.h
+
+../Generated_Code/TRIG_US_Back.h:
+
+../Generated_Code/PE_Types.h:
+
+../Generated_Code/PE_Error.h:
+
+../Generated_Code/PE_Const.h:
+
+../Generated_Code/IO_Map.h:
+
+/home/charly/workspace/medidorDistancia/Static_Code/IO_Map/MKL25Z4.h:
+
+/home/charly/workspace/medidorDistancia/Static_Code/PDD/GPIO_PDD.h:
+
+/home/charly/workspace/medidorDistancia/Static_Code/PDD/PDD_Types.h:
+
+../Generated_Code/Cpu.h:
