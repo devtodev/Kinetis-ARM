fc8880143b88b921954b0393d78187f3af4d0b70
CarlosMiguens <cmiguens@gmail.com>
2016-10-18 22:00:29 -0300

    varios

---
+++ Pathfinder/Sources/ultrasonido.c
@@ -0,0 +1,15 @@
+/*
+ * motor.c
+ *
+ *  Created on: 18/10/2016
+ *      Author: Carlos Miguens
+ */
+
+void getDistanceFront()
+{
+
+}
+
+void getDistanceBack()
+{
+}
---
+++ Pathfinder/Sources/ultrasonido.h
@@ -0,0 +1,9 @@
+/*
+ * motor.h
+ *
+ *  Created on: 18/10/2016
+ *      Author: Carlos Miguens
+ */
+
+void getDistanceFront();
+void getDistanceBack();
