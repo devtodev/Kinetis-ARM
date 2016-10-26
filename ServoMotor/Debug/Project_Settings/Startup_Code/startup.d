8e1a8dc97a733291c5c3b145704f9e850bd3a3c1
CarlosMiguens <cmiguens@gmail.com>
2016-10-26 19:46:22 -0300

    gitignore

--- ServoMotor/Debug/Project_Settings/Startup_Code/startup.d
+++
@@ -1,8 +0,0 @@
-Project_Settings/Startup_Code/startup.o: \
- ../Project_Settings/Startup_Code/startup.c \
- /home/karl/workspace.kds/Kinetis-ARM/ServoMotor/Generated_Code/IO_Map.h \
- /home/karl/workspace.kds/Kinetis-ARM/ServoMotor/Static_Code/IO_Map/MKL25Z4.h
-
-/home/karl/workspace.kds/Kinetis-ARM/ServoMotor/Generated_Code/IO_Map.h:
-
-/home/karl/workspace.kds/Kinetis-ARM/ServoMotor/Static_Code/IO_Map/MKL25Z4.h:
